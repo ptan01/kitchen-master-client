9ca687007a136042e68a985673d434e53e4b4886
ptan01 <ptanjim01@gmail.com>
2023-05-03 16:48:26 +0600

    lazy load add

--- src/pages/home/chefs/Chefs.jsx
+++ src/pages/home/chefs/Chefs.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazy-load';
 
 const Chefs = ({chef}) => {
 
 
     return (
         <div className="card md:card-side bg-base-100 shadow-xl">
-            <figure><img className='w-[300px] h-[300px]' src={chef.chefPicture} alt="chef img" /></figure>
+            <figure>
+                <LazyLoad height={300} ><img className='w-[300px] h-[300px]' src={chef.chefPicture} alt="chef img" /></LazyLoad>
+            </figure>
             <div className="card-body">
                 <h2 className="card-title">{chef.chefName}</h2>
                 <p className='m-0 p-0'>Experience:{chef.experience} Year</p>
