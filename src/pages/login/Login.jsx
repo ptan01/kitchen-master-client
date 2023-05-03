f0a64816f34cdad61e0c90b46dedac501d882ebb
ptan01 <ptanjim01@gmail.com>
2023-05-03 10:41:43 +0600

    login and registetion function add

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [error , setError] = useState('')
+    const [success, setSuccess] = useState('')
+
+    const handleLogin = (e)=>{
+        e.preventDefault()
+        const form = e.target ;
+        const email = form.email.value ;
+        const pass = form.password.value;
+        console.log(email, pass)
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-
+            <div className="hero-content flex-col ">
+                <div className='my-5'>
+                    <h2 className='text-4xl'>Please Login </h2>
+                </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
 
                         </div>
                         <div className="form-control mt-6">
@@ -27,7 +40,13 @@ const Login = () => {
                         <label className="label">
                             <span>New in Kitchen Master Please <Link className='text-red-700' to='/register'>Register</Link></span>
                         </label>
-                    </div>
+                        <label className="label">
+                           <span className='text-red-500'>{error}</span>
+                        </label>
+                        <label className="label">
+                           <span className='text-green-500'>{success}</span>
+                        </label>
+                    </form>
                 </div>
             </div>
         </div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+
+    const handleRegister = (e)=>{
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value ;
+        const pass = form.password.value ;
+        const name =form.name.value ;
+        const photo = form.photo.value ;
+        console.log(email, pass, name, photo)
+    }
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col ">
@@ -9,30 +22,30 @@ const Register = () => {
                     <h2 className='text-4xl my-5'>Please Register</h2>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form onSubmit={handleRegister} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder="Name" className="input input-bordered" />
+                            <input type="text" name='name' placeholder="Name" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Photo URL</span>
                             </label>
-                            <input type="text" placeholder="URL" className="input input-bordered" />
+                            <input type="text" name='photo' placeholder="URL" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
 
                         </div>
                         <div className="form-control mt-6">
@@ -41,7 +54,7 @@ const Register = () => {
                         <label className="label">
                             <span>Already Have an Account ? Please <Link className='text-red-700' to='/login'>Login</Link></span>
                         </label>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
